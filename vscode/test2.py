e4bb4f86255748917d2e9d2b6c9051a523cf807a
Ahmed Ihsan Ali <46008612+Ahmed-Ihsan@users.noreply.github.com>
2021-11-04 09:25:48 +0300

    Add files via upload

---
+++ vscode/test2.py
@@ -0,0 +1,45 @@
+# Import required libraries
+import pandas as pd
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Input, Output
+from jupyter_dash import JupyterDash
+import plotly.graph_objects as go
+
+JupyterDash.infer_jupyter_proxy_config()
+
+app = JupyterDash(__name__)
+
+airline_data = pd.read_csv('../airline_data.csv')
+
+app.layout = html.Div(children=[ html.H1('Airline Performance Dashboard', 
+                                style={'textAlign': 'center', 'color': '#503D36',
+                                'font-size': 40}),
+                                html.Div(["Input Year: ", dcc.Input(id='input-year', value='2010', 
+                                type='number', style={'height':'50px', 'font-size': 35}),], 
+                                style={'font-size': 40}),
+                                html.Br(),
+                                html.Br(),
+                                html.Div(dcc.Graph(id='line-plot')),
+                                ])
+
+# add callback decorator
+@app.callback( Output(component_id='line-plot', component_property='figure'),
+               Input(component_id='input-year', component_property='value'))
+
+# Add computation to callback function and return graph
+def get_graph(entered_year):
+    # Select 2019 data
+    df =  airline_data[airline_data['Year']==int(entered_year)]
+    
+    # Group the data by Month and compute average over arrival delay time.
+    line_data = df.groupby('Month')['ArrDelay'].mean().reset_index()
+
+    fig = go.Figure(data=go.Scatter(x=line_data['Month'], y=line_data['ArrDelay'], mode='lines', marker=dict(color='green')))
+    fig.update_layout(title='Month vs Average Flight Delay Time', xaxis_title='Month', yaxis_title='ArrDelay')
+    return fig
+
+# Run the app
+if __name__ == '__main__':
+    app.run_server(mode="inline", host="localhost")
